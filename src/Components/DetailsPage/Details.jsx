8b9fe3e8643314c3371910b87db32360d197b3d0
Humaira Hossain Methela <87987072+HHM-1999@users.noreply.github.com>
2026-07-07 16:40:23 +0600

    new ads added in details page

--- src/Components/DetailsPage/Details.jsx
+++ src/Components/DetailsPage/Details.jsx
@@ -3,7 +3,7 @@ import { toBengaliNumber } from 'bengali-number'
 import { format } from 'date-fns'
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
-// import waltonAds from '../../assets/media/Advertisement/300x250.jpg.jpeg'
+import Millionniere from '../../assets/media/Advertisement/Millionniere-scheme.png'
 import pubali from '../../assets/media/Advertisement/Pubali-Bank1.png'
 // import psi from '../../assets/media/Advertisement/psi.jpeg'
 // import psiPdf from '../../assets/Final __PSI_____6  X 4.pdf'
@@ -460,6 +460,16 @@ export default function Details() {
                                                         </a>
                                                     </div>
                                                 </div> */}
+                                                {/* =========advertisement for desktop version ============ */}
+                                                <div className="row">
+                                                    <div className="col-md-12 pb-3 ">
+                                                        <a href="https://www.agranibank.org/" target='blank'>
+                                                            <div className="DAdd1 d-flex  justify-content-center">
+                                                                <img src={Millionniere} alt="Advertisement" title="Advertisement" className="img-fluid img100" width={300} height={250} />
+                                                            </div>
+                                                        </a>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                         <div className="row">
@@ -476,7 +486,7 @@ export default function Details() {
                                                     {/* <div className="row">
                                                         <div className="col-md-12">
                                                             <div className="DAdd1 d-flex  justify-content-center">
-                                                                <img src={waltonAds} alt="Advertisement" title="Advertisement" className="img-fluid img100" width={300} height={250} />
+                                                                <img src={Millionniere} alt="Advertisement" title="Advertisement" className="img-fluid img100" width={300} height={250} />
                                                             </div>
                                                         </div>
                                                     </div> */}
@@ -485,13 +495,13 @@ export default function Details() {
                                         </div>
                                         {/* =========advertisement for mobile device============ */}
                                         <div className="row d-block d-lg-none">
-                                            {/* <div className="col-md-12 pb-3 ">
-                                                <a href="https://waltonbd.com/" target='blank'>
+                                            <div className="col-md-12 pb-3 ">
+                                                <a href="https://www.agranibank.org/" target='blank'>
                                                     <div className="DAdd1 d-flex  justify-content-center">
-                                                        <img src={waltonAds} alt="Advertisement" title="Advertisement" className="img-fluid img100" width={300} height={250} />
+                                                        <img src={Millionniere} alt="Advertisement" title="Advertisement" className="img-fluid img100" width={300} height={250} />
                                                     </div>
                                                 </a>
-                                            </div> */}
+                                            </div>
                                         </div>
                                         <div className="col-sm-12 d-print-none">
                                             <div className="row">
