a4195c633fe7f5b57ba3b58819840be2fc711f9f
Humaira Hossain Methela <87987072+HHM-1999@users.noreply.github.com>
2026-08-10 16:35:05 +0600

    new ads placement

--- src/Components/DetailsPage/Details.jsx
+++ src/Components/DetailsPage/Details.jsx
@@ -4,6 +4,7 @@ import { format } from 'date-fns'
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 // import Millionniere from '../../assets/media/Advertisement/Millionniere-scheme.png'
+import DesktopR2 from '../../assets/media/Advertisement/detailsAdsR2.jpg'
 import pubali from '../../assets/media/Advertisement/Pubali-Bank1.png'
 // import psi from '../../assets/media/Advertisement/psi.jpeg'
 // import psiPdf from '../../assets/Final __PSI_____6  X 4.pdf'
@@ -441,15 +442,15 @@ export default function Details() {
 
                                             <div className="col-lg-4 col-12 d-none d-lg-block detailsPage">
                                                 {/* =========advertisement for desktop version ============ */}
-                                                <div className="row">
+                                                {/* <div className="row">
                                                     <div className="col-md-12 pb-3 ">
-                                                        <a href="https://waltonbd.com/refrigerator-freezer" target='blank'>
+                                                        <a href="https://waltonbd.com/" target='blank'>
                                                             <div className="DAdd1 d-flex  justify-content-center">
                                                                 <img src={waltonAds} alt="Advertisement" title="Advertisement" className="img-fluid img100" width={300} height={250} />
                                                             </div>
                                                         </a>
                                                     </div>
-                                                </div>
+                                                </div> */}
                                                 <DCatLatest catLatest={catLatest} catName={catName.CategoryName} catSlug={catSlug} />
                                                 {/* <div className="row">
                                                     <div className="col-md-12 pb-3 ">
