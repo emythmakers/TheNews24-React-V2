c14e921e7e93f4679cdb96aeb064e776e5ea9a21
alamin2878 <alaminm305@gmail.com>
2026-09-06 17:30:08 +0600

    adv updated

--- src/Components/DetailsPage/Details.jsx
+++ src/Components/DetailsPage/Details.jsx
@@ -6,7 +6,7 @@ import { Link, useParams } from 'react-router-dom'
 // import Millionniere from '../../assets/media/Advertisement/Millionniere-scheme.png'
 // import DesktopR2 from '../../assets/media/Advertisement/detailsAdsR2.jpg'
 import pubali from '../../assets/media/Advertisement/Pubali-Bank1.png'
-import waltonAds from '../../assets/media/Advertisement/Fridge-Aktai-Walton_300-x-250.jpg (1).jpeg'
+// import waltonAds from '../../assets/media/Advertisement/Fridge-Aktai-Walton_300-x-250.jpg (1).jpeg'
 // import psi from '../../assets/media/Advertisement/psi.jpeg'
 // import psiPdf from '../../assets/Final __PSI_____6  X 4.pdf'
 import { banglaDateConvetar, ForLazyLoaderImg, scrollTop, timeAgo } from '../AllFunctions'
@@ -443,7 +443,7 @@ export default function Details() {
 
                                             <div className="col-lg-4 col-12 d-none d-lg-block detailsPage">
                                                 {/* =========advertisement for desktop version ============ */}
-                                                <div className="row">
+                                                {/* <div className="row">
                                                     <div className="col-md-12 pb-3 ">
                                                         <a href="https://waltonbd.com/refrigerator-freezer" target='blank'>
                                                             <div className="DAdd1 d-flex  justify-content-center">
@@ -451,7 +451,7 @@ export default function Details() {
                                                             </div>
                                                         </a>
                                                     </div>
-                                                </div>
+                                                </div> */}
                                                 <DCatLatest catLatest={catLatest} catName={catName.CategoryName} catSlug={catSlug} />
                                                 {/* <div className="row">
                                                     <div className="col-md-12 pb-3 ">
